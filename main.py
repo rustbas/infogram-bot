12d8f31b6f20f17271a4d2933ee82fcf6c587115
Basyrov Rustam <hrustbas@gmail.com>
2025-06-04 02:05:16 +0300

    remove hello function and init getinfo

--- main.py
+++ main.py
@@ -3,13 +3,22 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 from config import TOKEN
 
-async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+async def get_chat_info(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
+    chat_id = update.message.chat.id
+
+    reply_message = f"""
+    Information about this chat:
+
+Chat ID – "`{chat_id}`"
+    """
+
+    # await update.message.reply_text(f"Hello from python!")
+    await update.message.reply_text(reply_message, parse_mode='MarkdownV2')
 
 app = ApplicationBuilder().token(TOKEN).build()
 
-app.add_handler(CommandHandler("hello", hello))
+app.add_handler(CommandHandler("getinfo", get_chat_info))
 
 app.run_polling()
 
